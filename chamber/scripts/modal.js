c98a8d1d89796f189d9d9fa4e913f10ff4a4e438
agerockhub <agerock2013@gmail.com>
2025-11-22 02:28:13 +0000

    update

---
+++ chamber/scripts/modal.js
@@ -0,0 +1,69 @@
+const openButton = document.querySelector("#openButton");
+const dialogBox = document.querySelector("#dialogBox");
+const closeButton = document.querySelector("#closeButton");
+
+// *show the dialog* button opens the dialog modally
+openButton.addEventListener("click", () => {
+    dialogBox.showModal();
+});
+
+
+
+//*Close* button closes the dialog
+closeButton.addEventListener("click", () => {
+    dialogBox.close();
+})
+
+
+//SECOND
+const openButton1 = document.querySelector("#openButton1");
+const dialogBox1 = document.querySelector("#dialogBox1");
+const closeButton1 = document.querySelector("#closeButton1");
+
+// *show the dialog* button opens the dialog modally
+openButton1.addEventListener("click", () => {
+    dialogBox1.showModal();
+});
+
+
+
+//*Close* button closes the dialog
+closeButton1.addEventListener("click", () => {
+    dialogBox1.close();
+})
+
+
+//THIRD
+const openButton2 = document.querySelector("#openButton2");
+const dialogBox2 = document.querySelector("#dialogBox2");
+const closeButton2 = document.querySelector("#closeButton2");
+
+// *show the dialog* button opens the dialog modally
+openButton2.addEventListener("click", () => {
+    dialogBox2.showModal();
+});
+
+
+
+//*Close* button closes the dialog
+closeButton2.addEventListener("click", () => {
+    dialogBox2.close();
+})
+
+
+//FOURTH
+const openButton3 = document.querySelector("#openButton3");
+const dialogBox3 = document.querySelector("#dialogBox3");
+const closeButton3 = document.querySelector("#closeButton3");
+
+// *show the dialog* button opens the dialog modally
+openButton3.addEventListener("click", () => {
+    dialogBox3.showModal();
+});
+
+
+
+//*Close* button closes the dialog
+closeButton3.addEventListener("click", () => {
+    dialogBox3.close();
+})
