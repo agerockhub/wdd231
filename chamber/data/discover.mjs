639d116f5989000405b196823d753fa50ab89d22
agerockhub <agerock2013@gmail.com>
2025-11-30 02:09:28 +0000

    update

--- chamber/data/discover.mjs
+++ chamber/data/discover.mjs
@@ -1,49 +1,57 @@
 export const accraLandmarks = [
     {
         "name": "Black Star Gate",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/4/4b/Black_Star_Gate_%2821712600269%29.jpg",
+        "image": "./images/black-star-gate.webp",
+        "lazy": "lazy",
         "description": "A national monument located at Independence Square that symbolizes Ghana’s independence and sovereignty.",
         "location": "Independence Square, Accra"
     },
     {
         "name": "Independence Arch",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/5/53/Independence_Square%2C_Accra%2C_Ghana.JPG",
+        "image": "./images/independence-arch.webp",
+        "lazy": "lazy",
         "description": "A triumphal arch built to commemorate Ghana's independence. It stands within Independence Square.",
         "location": "Independence Square, Accra"
     },
     {
         "name": "Kwame Nkrumah Mausoleum",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Kwame_nkrumah_grave_accra_ghana.jpg/1200px-Kwame_nkrumah_grave_accra_ghana.jpg",
+        "image": "./images/kwame-nkrumah-mausoleum.webp",
+        "lazy": "lazy",
         "description": "A memorial park and museum dedicated to Ghana’s first president, featuring fountains, sculptures, and historical exhibits.",
         "location": "Central Accra"
     },
     {
         "name": "Jamestown Lighthouse",
-        "image": "https://viewghana.com/wp-content/uploads/2025/02/James_town_Lighthouse_accra2.jpg",
+        "image": "./images/james-town-lighthouse.webp",
+        "lazy": "lazy",
         "description": "A historic lighthouse offering panoramic views of the coast, located within the culturally rich Jamestown district.",
         "location": "Jamestown, Accra"
     },
     {
         "name": "Labadi Beach",
-        "image": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/ba/18/bb/dsc-0786-largejpg.jpg",
+        "image": "./images/labardi-beach.webp",
+        "lazy": "lazy",
         "description": "One of Accra’s most popular beaches, known for relaxation, horseback riding, music performances, and beachfront activities.",
         "location": "Labadi, Accra"
     },
     {
         "name": "W.E.B. Du Bois Center",
-        "image": "https://images.squarespace-cdn.com/content/v1/5d03dea6992449000176121b/1628147887049-0PR5Y5YURQVUEJ1VGXLC/IMG_8395.jpg",
+        "image": "./images/dubois-center.webp",
+        "lazy": "lazy",
         "description": "A museum, library, and memorial center dedicated to the life and Pan-African legacy of Dr. W.E.B. Du Bois.",
         "location": "Cantonments, Accra"
     },
     {
         "name": "Makola Market",
-        "image": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/49/c5/01/makola.jpg",
+        "image": "./images/markola-market.webp",
+        "lazy": "lazy",
         "description": "A bustling market in central Accra known for wholesale and retail goods, local fabrics, produce, and vibrant trading activity.",
         "location": "Central Accra"
     },
     {
         "name": "Holy Trinity Cathedral",
-        "image": "https://visaliv.s3.ap-south-1.amazonaws.com/Holy-Trinity-Cathedral-in-Ghana.jpg",
+        "image": "./images/holy-trinity-cathederal.webp",
+        "lazy": "lazy",
         "description": "A historic Anglican cathedral built in 1894, known for its colonial architecture and spiritual significance.",
         "location": "High Street, Accra"
     }
